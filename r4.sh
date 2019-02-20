796b9a0abe441619131849aa69206b53f6fc986d
simonmajor <5021548+simonmajor@users.noreply.github.com>
2019-02-20 20:03:13 +0000

    T5088 - dirsrv stays down

--- r4.sh
+++ r4.sh
@@ -35,25 +35,19 @@ yum -y --setopt=multilib_policy=best --exclude='*.i686' downgrade centos-release
 sleep 2
 sync
 sleep 2
+systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
+sleep 10
 yum -y --setopt=multilib_policy=best --setopt=obsoletes=0 --exclude='*.i686' --skip-broken update
 sleep 2
 sync
 sleep 2
+systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
+sleep 10
 yum -y --setopt=multilib_policy=best --exclude='*.i686' --skip-broken upgrade
 sleep 2
 sync
 sleep 2
 systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
-echo Sleeping for 131 then shutting down FreeIPA
-sleep 131
-sync ; ipactl stop ; sync
-sleep 2
-systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
-sleep 10
-systemctl stop {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
-sleep 10
-systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
-echo Sleeping for 61
 sleep 61
 sync
 echo Rebooting
