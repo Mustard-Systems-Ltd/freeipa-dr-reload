ac69ad4a56030a8039f6244f3bdb512a844fc9d0
simonmajor <5021548+simonmajor@users.noreply.github.com>
2019-02-20 18:21:20 +0000

    T5088 - draft C7.5.1804 refactor

--- r4.sh
+++ r4.sh
@@ -9,19 +9,23 @@ sync ; ipactl stop ; sync
 sleep 2
 systemctl --lines=0 status {dirsrv@${realmm},httpd,ipa-dnskeysyncd,ipa_memcached,kadmin,krb5kdc,named-pkcs11,pki-tomcatd@pki-tomcat}.service
 sleep 10
+yum -y --setopt=multilib_policy=best --exclude='*.i686' --enablerepo=base --enablerepo=extras --enablerepo=updates update centos-release
+sleep 2
 yum-config-manager --disable C7.2.1511-base
 sleep 2
 yum-config-manager --disable C7.2.1511-extras
 sleep 2
 yum-config-manager --disable C7.2.1511-updates
 sleep 2
-yum-config-manager --enable base
+yum-config-manager --enable C7.5.1804-base
 sleep 2
-yum-config-manager --enable extras
+yum-config-manager --enable C7.5.1804-extras
 sleep 2
-yum-config-manager --enable updates
+yum-config-manager --enable C7.5.1804-updates
 sleep 2
 yum makecache
+sleep 2
+#yum -y --setopt=multilib_policy=best --exclude='*.i686' downgrade centos-release-7-2.1511.el7.centos.2.10
 sleep 2
 yum -y --setopt=multilib_policy=best --setopt=obsoletes=0 --exclude='*.i686' --skip-broken update
 sleep 2
@@ -50,4 +54,4 @@ echo Sleeping for 61
 sleep 61
 sync
 echo Rebooting
-sync ; ipactl stop ; sync ; exec shutdown -r now
+#sync ; ipactl stop ; sync ; exec shutdown -r now
