64d6f5205c9de9b1fa19745787abfdc98dd76dd3
simonmajor <5021548+simonmajor@users.noreply.github.com>
2019-03-05 14:39:14 +0000

    T5157 - grub done

--- ssh-remote-replica-4676.sh
+++ ssh-remote-replica-4676.sh
@@ -26,34 +26,49 @@ remote()
 	ssh -o IdentityAgent=none -o PreferredAuthentications=publickey -o ConnectTimeout=8 root@${rip} -- $@
 }
 
+rscp()
+{
+	scp -p -o IdentityAgent=none -o PreferredAuthentications=publickey -o ConnectTimeout=8 ${1} root@${rip}:${1}
+}
+
 remote nmcli connection modify eth0 ipv4.dns \"$(ip route get 8.8.8.8 | awk '$(NF-1) == "src" { print $NF }')\"
 remote nmcli connection modify eth0 ipv4.dns-search "${bzn}"
 remote hostnamectl set-hostname $(remote hostname | sed -e 's/^\([^.]*\)\..*$/\1/').${bzn}
 remote exec systemctl restart network.service
 sleep 3
-#remote getenforce 
-#remote setenforce 0
-#remote getenforce 
-#remote echo "'"dirsrv soft nofile 4096"'" \> /etc/security/limits.d/71-mustard-dirsrv.conf
-#remote echo "'"dirsrv hard nofile 8192"'" \>\> /etc/security/limits.d/71-mustard-dirsrv.conf
-#remote \[\[ -d /etc/systemd/journald.conf.d \]\] \|\| mkdir /etc/systemd/journald.conf.d
-#remote echo "'"\[Journal\]
-#remote Storage=persistent
-#remote SystemMaxUse=250M
-#remote MaxRetentionSec=13month"'" \> /etc/systemd/journald.conf.d/mustard_recommeds.conf
-#remote systemctl restart systemd-journald.service
+remote getenforce 
+remote setenforce 0
+remote getenforce 
+remote echo "'"dirsrv soft nofile 4096"'" \> /etc/security/limits.d/71-mustard-dirsrv.conf
+remote echo "'"dirsrv hard nofile 8192"'" \>\> /etc/security/limits.d/71-mustard-dirsrv.conf
+remote \[\[ -d /etc/systemd/journald.conf.d \]\] \|\| mkdir /etc/systemd/journald.conf.d
+echo '[Journal]
+Storage=persistent
+SystemMaxUse=250M
+MaxRetentionSec=13month' > /tmp/mustard_recommeds.conf.$$
+rscp /tmp/mustard_recommeds.conf.$$
+rm -f /tmp/mustard_recommeds.conf.$$
+remote mv /tmp/mustard_recommeds.conf.$$ /etc/systemd/journald.conf.d/mustard_recommeds.conf
+remote systemctl restart systemd-journald.service
 sleep 2
-#remote sed -i -e "'"/^GRUB_SERIAL_COMMAND/d"'" -e "'"/^GRUB_TERMINAL_OUTPUT=/{s/=".*$/="console serial"/;a\
-#remote GRUB_SERIAL_COMMAND="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1"
-#remote }"'" /etc/default/grub
-#remote grep -q "'"^GRUB_CMDLINE_LINUX.*console=ttyS"'" /etc/default/grub \|\| sed -i -e "'"/^GRUB_CMDLINE_LINUX=/s/="/="console=tty0 console=ttyS0,115200n8 /"'" -e "'"s/ rhgb quiet"$/"/"'" /etc/default/grub
-#remote grub2-mkconfig -o /boot/grub2/grub.cfg
+echo '/^GRUB_SERIAL_COMMAND/d' > /tmp/sed1.$$
+echo '/^GRUB_TERMINAL_OUTPUT=/{s/=".*$/="console serial"/;a\
+GRUB_SERIAL_COMMAND="serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1"
+}' >> /tmp/sed1.$$
+rscp /tmp/sed1.$$
+rm -f /tmp/sed1.$$
+remote sed -i -f /tmp/sed1.$$ /etc/default/grub
+remote rm -f /tmp/sed1.$$
+remote grep -q "'"'^GRUB_CMDLINE_LINUX.*console=ttyS'"'" /etc/default/grub \|\| sed -i -e "'"'/^GRUB_CMDLINE_LINUX=/s/="/="console=tty0 console=ttyS0,115200n8 /'"'" -e "'"'s/ rhgb quiet"$/"/'"'" /etc/default/grub
+remote grub2-mkconfig -o /boot/grub2/grub.cfg
+remote cat /etc/default/grub
 sleep 2
-#remote yum makecache
+remote yum makecache
 sleep 2
-#remote yum -y --setopt=multilib_policy=best --exclude="'"*.i686"'" update
+remote yum -y --setopt=multilib_policy=best --exclude="'"'*.i686'"'" update
 sleep 2
-#remote yum -y --setopt=multilib_policy=best --exclude="'"*.i686"'" install yum-versionlock yum-utils
+exit 0
+#remote yum -y --setopt=multilib_policy=best --exclude="'"'*.i686'"'" install yum-versionlock yum-utils
 sleep 2
 #remote yum -y --setopt=obsoletes=0 install ipa-server-4.6.4-10.el7.centos.2 ipa-server-dns-4.6.4-10.el7.centos.2
 sleep 2
@@ -65,7 +80,7 @@ sleep 2
 sleep 2
 #remote yum -y --setopt=multilib_policy=best --setopt=obsoletes=0 --exclude="'"*.i686"'" --skip-broken update
 sleep 2
-#remote yum -y --setopt=multilib_policy=best --exclude="'"*.i686"'" --skip-broken upgrade
+#remote yum -y --setopt=multilib_policy=best --exclude="'"'*.i686'"'" --skip-broken upgrade
 sleep 2
 #remote yum -y --setopt=obsoletes=0 install epel-release
 sleep 2
